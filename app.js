55b4a6701dfe010b024e8686a2391ca9fbc1f59f
M0H1K4N <nejer911@gmail.com>
2024-01-06 23:06:19 +0400

    learning methods

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/app", (req, res) => {
+  res.setHeader('Contentt-Type', 'application/json').send("Hello World!");
+}); 
 
+app.post("/", (req, res) => {
+    res.setHeader('Contentt-Type', 'application/json').send("Hello World!");
+  }); 
+
+  app.put("/", (req, res) => {
+    res.setHeader('Contentt-Type', 'application/json').send("Hello World!" );
+  });
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
