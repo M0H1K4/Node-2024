17d88a6e9ccf46a8241875af881d55a3e51d7a7a
M0H1K4N <nejer911@gmail.com>
2024-01-07 16:22:23 +0400

    make code leaner

--- app.js
+++ app.js
@@ -14,14 +14,17 @@ const apps = [
   { id: 4, name: "App 4" },
   { id: 5, name: "App 5" },
 ];
-console.log(apps[0]);
+console.log(apps[0].name);
 
 app.get("/app", (req, res) => {
   res.json(apps);
 });
 
 app.get("/app/:id", (req, res) => {
-  const result = apps.find((app) => app.id === pareseInt(req.params.id));
+  const vId = pareseInt(req.params.id);
+  const result = apps.find((app) => {
+    return app.id === vId;
+  });
   res.json(result);
 });
 
