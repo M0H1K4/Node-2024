3197d287d697932853f3d6b1e91f3b24bda8f9b0
M0H1K4N <nejer911@gmail.com>
2024-01-07 15:49:29 +0400

    Adding array

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+app.get('/app', (req, res) => {
+const apps = [
+  {id: 1, name: 'App 1' },
+  {id: 2, name: 'App 2'},
+  {id: 3, name: 'App 3'},
+  {id: 4, name: 'App 4'},
+  {id: 5, name: 'App 5'}
+]
+res.json(apps);
+});
+
 app.get("/app", (req, res) => {
-  res.setHeader('Contentt-Type', 'application/json').send("Hello World!");
+ res.json({id: req.params.id, name: 'App 1'})
 }); 
 
 app.post("/", (req, res) => {
