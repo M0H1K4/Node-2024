7d2e1d5c2a909b39941e79184dda1c0d5b8c4cda
M0H1K4N <nejer911@gmail.com>
2024-01-07 16:13:58 +0400

    as

--- app.js
+++ app.js
@@ -2,30 +2,37 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const vApp = {
+  id: 1,
+  name: "App 1",
+};
 
-app.get('/app', (req, res) => {
 const apps = [
-  {id: 1, name: 'App 1' },
-  {id: 2, name: 'App 2'},
-  {id: 3, name: 'App 3'},
-  {id: 4, name: 'App 4'},
-  {id: 5, name: 'App 5'}
-]
-res.json(apps);
-});
+  { id: 1, name: "App 1" },
+  { id: 2, name: "App 2" },
+  { id: 3, name: "App 3" },
+  { id: 4, name: "App 4" },
+  { id: 5, name: "App 5" },
+];
+console.log(apps[0]);
 
 app.get("/app", (req, res) => {
- res.json({id: req.params.id, name: 'App 1'})
-}); 
+  res.json(apps);
+});
 
-app.post("/", (req, res) => {
-    res.setHeader('Contentt-Type', 'application/json').send("Hello World!");
-  }); 
+app.get("/app/:id", (req, res) => {
+  const result = apps.find((app) => app.id === pareseInt(req.params.id));
+  res.json(result);
+});
 
-  app.put("/", (req, res) => {
-    res.setHeader('Contentt-Type', 'application/json').send("Hello World!" );
-  });
-  
+app.post("/app/:id", (req, res) => {
+  res.setHeader("Contentt-Type", "application/json").send("Hello World!");
+});
+
+app.put("/ap", (req, res) => {
+  res.setHeader("Contentt-Type", "application/json").send("Hello World!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
