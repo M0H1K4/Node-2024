41880168cf577b9b856c76f043b4f7bc8bac715f
M0H1K4N <nejer911@gmail.com>
2024-01-07 20:35:08 +0400

    ADD

--- app.js
+++ app.js
@@ -57,7 +57,20 @@ app.put("/app/:id", (req, res) => {
   });
 });
 
-app.get("index", (req, res) => {
+app.delete("/app/:id", (req, res) => {
+  const vId = parseInt(req.params.id);
+  const result = apps.find((app) => {
+    return app.id === vId;
+  });
+  const index = apps.indexOf(result);
+  apps.splice(index, 1);
+  res.json({
+    status: true,
+    message: "განცხადება წაიშალა წარმატებით!",
+    result: result,
+  });
+});
+app.get("/index", (req, res) => {
   res.status(200).sendFile(__dirname + "/views/index.html");
 });
 
